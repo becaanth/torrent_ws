acac07d2c6a6495070708af5dcfb252edc5ffc68
Anthony Beca <anthony.beca@robotics.utias.utoronto.ca>
2026-08-22 15:15:02 -0400

    (orch) enable handle_torrent_update

--- scripts/orchestrator.py
+++ scripts/orchestrator.py
@@ -161,7 +161,7 @@ class Orchestrator:
 
     def handle_torrent_update(self, robot_id, mutable_item):
         logging.info(f"handle_torrent_update for id {robot_id}")
-        # self.fleet[robot_id].update_mutable_item(mutable_item) # TODO: ANTHONY UNCOMMENT THIS
+        self.fleet[robot_id].update_mutable_item(mutable_item) # TODO: ANTHONY UNCOMMENT THIS
 
     def run(self):
         logging.info("[agent.run]")
