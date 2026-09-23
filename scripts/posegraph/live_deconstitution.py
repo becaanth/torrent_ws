c22664e4e43f8b80f0e7d4518f50c49eb9056455
Anthony Beca <anthony.beca@robotics.utias.utoronto.ca>
2026-09-23 15:35:44 -0400

    feat(dec): debug

--- scripts/posegraph/live_deconstitution.py
+++ scripts/posegraph/live_deconstitution.py
@@ -320,6 +320,7 @@ class Deconstitutor:
                 # Only inspect edges connected to this local submap TODO: resolve merging
                 ingress_id = self._from_ids[e_mask][0] # vertex from incoming edge
                 egress_id = self._to_ids[e_mask][-1] # vertex at outgoing edge
+                logging.debug(f"ingress: {ingress_id}, egress: {egress_id}")
                 
                 if extract_robot_id(ingress_id) != extract_robot_id(egress_id):
                     # ingress/egress came from different robots. this is a merge to remote
